225e3779ceb3a82816f657bdd2e61679a40040bb
Mauro Tanaka <mnoriot@gmail.com>
2024-02-08 17:37:34 -0300

    Challenge1 (Done)

---
+++ Aulas_C_Sharp/Aulas_C_Sharp/Exercicio 1.cs
@@ -0,0 +1,7 @@
+namespace Aulas_C_Sharp
+{
+    public class Exercicio_1
+    {
+        
+    }
+}
---
+++ Aulas_C_Sharp/Aulas_C_Sharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Aulas_C_Sharp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Exercicio2 MyObject = new Exercicio2();
+            
+            //MyObject.MyMethod();
+
+            Challenge1 _challenge1 = new Challenge1();
+            
+            _challenge1.ExibirOpcoesDoMenu();
+
+        }
+    }
+}
